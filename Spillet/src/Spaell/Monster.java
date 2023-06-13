f91574425418f6bfa4ae914f9419b5978faf96da
JWoodh <jowoa002@osloskolen.no>
2023-06-13 11:55:27 +0200

    Unødvendig kode??😰 borte nå😋

--- Spillet/src/Spaell/Monster.java
+++ Spillet/src/Spaell/Monster.java
@@ -4,11 +4,9 @@ public class Monster {
     private String[] races = {"berater", "boggler", "swatter", "stomper","apostle" ,"basilisk"}; //Navnene til monsterne man møter
     private int[] damages = {3,6, 25, 22, 28, 25};// Skaden monstrene gjør
     private int[] healths = {30,45,25,60,96, 160};// Hvor mye liv monstrene har
-    private String[] sprites = {"berater.png", "boggler.png", null, "stomper.png", "apostle.png", "basilisk.png"};
     private String race;
     private int damage;
     private int health;
-    private String sprite;
     
     Monster(int i) {
         if (i==-1){
@@ -17,7 +15,6 @@ public class Monster {
         race = races[i];
         damage = damages[i];
         health = healths[i];
-        sprite = sprites[i];
     }
 
     //Lager setters og getters for variablene
@@ -36,10 +33,6 @@ public class Monster {
 
     public int getHealth() {
         return health;
-    }
-
-    public String getSprite() {
-        return sprite;
     }
 
     public String[] getRaces() {
